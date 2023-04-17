2f16992b68a363ab3acbb095eeb74b7f8488c26d
Tobias Gies <tobias@tobiasgies.de>
2023-04-17 16:12:39 +0200

    Enable TZCONVERT custom function to work with regions

--- CustomFunctions.ts
+++ CustomFunctions.ts
@@ -1,12 +1,14 @@
 /**
  * Converts the input datetime object to the supplied timezone and formats it with the supplied format string.
  *
- * @param {Date} datetime The datetime to format
+ * @param {Date|Array<Array<Date>>} datetime The datetime to format
  * @param {string} timezone The timezone to convert to
  * @param {string} format The format string for the output
  * @return The formatted string in the supplied timezone.
  * @customfunction
  */
-function TZCONVERT(datetime: Date, timezone: string, format: string): string {
-    return Utilities.formatDate(datetime, timezone, format);
+function TZCONVERT(datetime: Date|Array<Date>|Array<Array<Date>>, timezone: string, format: string): string|Array<Array<string>> {
+    return Array.isArray(datetime)
+        ? datetime.map(it1 => it1.map(it2 => Utilities.formatDate(it2, timezone, format)))
+        : Utilities.formatDate(datetime, timezone, format);
 }
